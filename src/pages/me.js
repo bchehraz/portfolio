194d9d98a647b01d3b1386aa7bcc4f621e20d2cf
Babak Chehraz <bchehraz@csumb.edu>
2019-10-13 18:43:42 -0700

    Updated information on 'me' page

--- src/pages/me.js
+++ src/pages/me.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link, graphql } from 'gatsby';
-import Img from 'gatsby-image';
+import PropTypes from 'prop-types';
+// import Img from 'gatsby-image';
 import styled from 'styled-components';
 import Layout from '../components/layout';
 
@@ -15,7 +16,7 @@ const Container = styled.div`
   padding: 2em;
 `;
 
-const MePage = ({ data }) => {
+const MePage = () => {
   return (
     <Layout>
       <Container>
@@ -27,37 +28,14 @@ const MePage = ({ data }) => {
           {" "}
         </p>
         <p>
-          My goal with this website is to show my personal and professional coding experience, as well as <Link to="/learning"><b>{"what I'm learning"}</b></Link>.
+          My goal with this website is to show my personal and professional coding experience, as well as <Link to="/learning"><b>{"what I'm learning"}</b></Link> and <Link to="/links"><b>Links to Useful Sites</b></Link>.
         </p>
         <h3>Personal Life</h3>
-        <p>{"I love the outdoors. I try to visit Yosemite every chance I get. Hiking and kayaking are where I am at peace. If I'm not outside, I'll either be coding, reading, listening to podcasts, or practicing guitar. "}
-        {"I've been a huge gamer for most of my life and I've slowly begun to part ways with games because I would like to focus my efforts to the things in life that matter. That said, I must give credit to video games for leading me toward a career in coding!"}</p>
-
-
-        <h3>Personal Challenges</h3>
-        <p>
-          {"I've had to overcome some difficult challenges personally since going off to college, like intense anxiety regarding my health. "}
-          {"Since graduating, I've taken it upon myself to learn everything about the food I was eating to try and explain why I was feeling the way I did. "}
-          {"Since putting in the effort to learn about these topics, I did what I thought I couldn't do. I lost 100 lbs and began to exercise regularly again. "}
-          {"What a journey it has been. Learning about my diet changed my perspective on the various ways food affects can affect a person, not just physically but mentally, as part of an individual's environment. "}
-          {"This inspired me to begin developing projects tailored to helping anyone meet their personal goals. A little bit of effort everyday goes a long way!"}
-
-        </p>
+        <p>{"I love being out in nature. I try to visit Yosemite every chance I get. Hiking and kayaking are where I am at peace. If I'm not exploring, I'll either be coding, reading, listening to podcasts, or practicing guitar. You might catch me at the local coffee shop working on a project!"}
+        {""}</p>
 
         <p><b>Thanks for stopping by and have a wonderful day :)</b></p>
 
-        <p>{"Now here's me with crazy hair and heavy winds at a beach near Big Sur, CA."}</p>
-        <Img
-          fluid={data.aboutPicture.childImageSharp.fluid}
-          alt="Photo of Babak"
-          style={{
-            width: '100%',
-            height: '100%',
-            display: 'flex',
-            justifyContent: 'center',
-            margin: '0 auto',
-          }}
-        />
         {
           //insert image of self here
 
@@ -77,6 +55,26 @@ const MePage = ({ data }) => {
     </Layout>
   );
 };
+
+MePage.propTypes = {
+  data: PropTypes.object.isRequired,
+}
+
+/*
+
+<Img
+  fluid={data.aboutPicture.childImageSharp.fluid}
+  alt="Photo of Babak"
+  style={{
+    width: '100%',
+    height: '100%',
+    display: 'flex',
+    justifyContent: 'center',
+    margin: '0 auto',
+  }}
+/>
+
+*/
 
 export const query = graphql`
   query {
