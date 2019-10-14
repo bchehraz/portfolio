6aed2dca8a47c73eddf97ef3afbf44de0179f577
Babak Chehraz <bchehraz@csumb.edu>
2019-10-13 18:40:02 -0700

    Removed unnecessary dependencies, updatedd typography.js

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,26 +1,6 @@
 import Typography from 'typography';
-import funstonTheme from 'typography-theme-funston';
-import stowLakeTheme from 'typography-theme-stow-lake';
-import grandViewTheme from 'typography-theme-grand-view';
-import kirkham from 'typography-theme-kirkham';
-import moraga from 'typography-theme-moraga';
+import alton from 'typography-theme-alton';
 
-const typography = new Typography(moraga);
-
-// const typography = new Typography({
-//   baseFontSize: '18px',
-//   baseLineHeight: 1.666,
-//   headerFontFamily: [
-//     'Avenir Next',
-//     'Helvetica Neue',
-//     'Segoe UI',
-//     'Helvetica',
-//     'Arial',
-//     'sans-serif',
-//   ],
-//   bodyFontFamily: ['Georgia', 'serif'],
-// });
-
-// const typography = new Typography(funstonTheme);
+const typography = new Typography(alton);
 
 export default typography;
